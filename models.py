9f88d08c487d6069bb074d43a75b11d3d8982c2a
Puneet398 <143024333+Puneet398@users.noreply.github.com>
2025-10-21 03:38:19 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,4 +9,13 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), default='user')
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
 
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    s3_key = db.Column(db.String(300), nullable=False)
+    source = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
